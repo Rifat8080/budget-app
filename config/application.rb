9ee04641e40713ad0ebd4357bb1d11dfe4a1beb4
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-11 21:33:00 +0600

    fixed webservice

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module BudgetApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.require_master_key = true
 
     # Configuration for the application, engines, and railties goes here.
     #
